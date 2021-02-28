2c1e820965f5e81cb7dc672943453b4cb7c24496
Abhishek gupta <abhishek.9867.338961@gmail.com>
2021-02-28 07:28:17 +0530

    icon added

---
+++ app.js
@@ -0,0 +1,8 @@
+const nameLink = document.querySelector('#nameLink')
+nameLink.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+})
