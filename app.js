4b690e08aad13eb8ea91d1c35a2c8f44ffb3b591
Abhishek gupta <abhishek.9867.338961@gmail.com>
2021-03-02 08:09:14 +0530

    sideBar added

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const nameLink = document.querySelector('#nameLink')
+const icons = document.querySelector(".icons")
+const sidebar = document.querySelector(".sideBar-responsive")
+let isClick = true
+
 nameLink.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: "smooth"
     });
+})
+
+icons.addEventListener('click', (evt) => {
+    if (isClick) {
+        sidebar.classList.add('sideBar-click');
+        icons.classList.remove('fa-align-justify')
+        icons.classList.add('fa-times')
+        isClick = !isClick;
+    } else {
+        sidebar.classList.remove('sideBar-click');
+        icons.classList.add('fa-align-justify')
+        icons.classList.remove('fa-times')
+        isClick = !isClick;
+    }
+
 })
